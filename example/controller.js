7225f79eff054834b9463282f3b17418cee3d1cf
Yazhong Liu <yorkiefixer@gmail.com>
2014-10-19 20:30:54 +0800

    add .exec

--- example/controller.js
+++ example/controller.js
@@ -1,17 +1,33 @@
 
+var pg = require('pg');
+var sqoosel = require('../');
 var User = require('./models/user');
 var Tweet = require('./models/tweet');
 
 function addUser(username) {
-  var q = User.insert({username: username}).toQuery();
-  console.log(q);
+  User.insert({username: username})
+  .exec(function(err, result) {
+    // insert successfully
+  });
 }
 
 function findUser(username) {
-  var q = User.select().from()
-    .where({username: username}).order(User.createdAt.desc).toQuery();
-  console.log(q);
+  User.select()
+  .where({username: username})
+  .order(User.createdAt.desc)
+  .limit(100)
+  .exec(function(err, result) {
+    // get result
+  });
 }
+
+sqoosel({
+  query: function(exec) {
+    pg.connect('postgres://username:password@localhost/database', function(err, client, close) {
+      exec(err, client.query, close);
+    });
+  }
+});
 
 addUser('yorkie');
 findUser('yorkie');
